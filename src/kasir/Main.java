76a07d19e029f610c39c65edb26d3eaa91ee445a
wilma <mornlitoralis@gmail.com>
2026-07-05 21:06:36 +0700

    finishing Main file

--- src/kasir/Main.java
+++ src/kasir/Main.java
@@ -1,5 +1,28 @@
 package kasir;
 
+import kasir.Views.login;
+import kasir.Views.menuUtama;
+import kasir.Model.User;
+import javax.swing.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Memulai Aplikasi Kasir Toko Emas...");
+        
+        login formLogin = new login();
+        User kasirAktif = formLogin.tampilkanFormLogin();
+        
+        if (kasirAktif != null) {
+            System.out.println("Kasir aktif saat ini: " + kasirAktif.getNamaKasir());
+                
+            menuUtama menu = new menuUtama(kasirAktif);
+            menu.tampilkanMenu();
+            
+            JOptionPane.showMessageDialog(null, "Sesi kerja selesai. Sampai jumpa!");
+            System.exit(0);
+        } else {
+            System.out.println("Aplikasi ditutup dari halaman login.");
+            System.exit(0);
+        }
+    }
 }
